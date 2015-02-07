f812ba90012764235eaf46637c21edc5e9cc225e
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-07 16:24:16 -0500

    define change

--- Defining Variables/script.js
+++ Defining Variables/script.js
@@ -2,5 +2,9 @@
  * Created by tabithawestfall on 2/7/15.
  */
 
-var age;
-age = 20;
+var age; //declaration
+age = 20; //definition
+
+//storage container = value
+
+var yearOfBirth = 1994; //declaration and definition on same line
