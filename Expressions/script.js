1d8b70585b77208c7fd9b4e5b2c72c5b9f6b1dfa
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-08 13:16:13 -0500

    math expressions
    area of triangle

--- Expressions/script.js
+++ Expressions/script.js
@@ -5,13 +5,20 @@
 //Expressions
 
 //var a = 2; // sets up our variable a and defines it with 2
-
-
 //a = a + 3; //add 3 to a
 //console.log(a);
 
-var yearBorn = 1994;
 
-var age = 2015 - yearBorn - 1;
 
-console.log(age);
+//var yearBorn = 1994;
+//var age = 2015 - yearBorn - 1;
+//console.log(age);
+
+
+
+//area of the triangle is half of the width times the height
+
+var width = 7;
+var height = 8;
+var area = width * height/2;
+console.log(area);
