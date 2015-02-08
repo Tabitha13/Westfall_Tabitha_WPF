46bae3ef4cc4d2be4542c88b0ac1dc72337fad3d
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-08 13:33:56 -0500

    arrays in expressions

--- Expressions/script.js
+++ Expressions/script.js
@@ -29,14 +29,19 @@
 
 
 //order of operations
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+//var quiz1 = 87;
+//var quiz2 = 100;
+//var quiz3 = 60;
+//var quiz4 = 80;
 //the average is the sum of items divided by number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 //console.log(average);
-var length = 7;
-var width = 6;
-var perimeter =  length * 2 + width * 2;
-console.log(perimeter);
+//var length = 7;
+//var width = 6;
+//var perimeter =  length * 2 + width * 2;
+//console.log(perimeter);
+
+//arrays in expressions
+var orangeBins = [234, 567, 883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
