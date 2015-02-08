204a33f828b1e4f239825e95971337c40254c781
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-08 13:21:06 -0500

    Remainder

--- Expressions/script.js
+++ Expressions/script.js
@@ -18,7 +18,11 @@
 
 //area of the triangle is half of the width times the height
 
-var width = 7;
-var height = 8;
-var area = width * height/2;
-console.log(area);
+//var width = 7;
+//var height = 8;
+//var area = width * height/2;
+//console.log(area);
+
+//modulo operator
+var remainder = 32 % 2;
+console.log(remainder);
