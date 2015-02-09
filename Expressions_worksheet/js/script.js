10f87c0358ebf449f657367caa15d309c8f431c1
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 12:49:25 -0500

    Dog Years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,5 @@
+// Tabitha Westfall February 9th 2015 Expressions Worksheet
+//Dog Years
+var humanYears = 5; //sparky is 5 years old in human years (given)
+var dogYears = 7 * humanYears; //equation to calculate sparkys age in dog years using constant of 7
+console.log(dogYears);
