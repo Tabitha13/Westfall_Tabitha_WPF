2b3b0d21f4f3cb794c35fe30f7ac2e1dbdcc55ad
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 13:25:52 -0500

    Slices of Pie part 2

--- Expressions_worksheet/Slice of Pie Part 2/js/script.js
+++ Expressions_worksheet/Slice of Pie Part 2/js/script.js
@@ -4,4 +4,4 @@ var pizzaSlices = 10; //variable of how many slices there are in each pizza
 var guests = 14; //variable of how many guests there are
 var pizzaOrdered = 5; // variable of how many pizzas were ordered
 var remainder = pizzaSlices * pizzaOrdered % guests; //equation to determine how many slices sparky will get
-console.log ("Sparky got " + remainder + " slices of pizza.");
+console.log ("Sparky got " + remainder + " slices of pizza."); //prints answer to console
