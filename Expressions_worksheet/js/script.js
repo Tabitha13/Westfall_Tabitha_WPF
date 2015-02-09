b6b7c70950098cca55ee2a3ca60d19d6dc25dd72
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 13:09:14 -0500

    Slice of Pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,15 +1,15 @@
 // Tabitha Westfall February 9th 2015 Expressions Worksheet
 //Dog Years
-var humanYears = 5; //sparky is 5 years old in human years (given)
-var dogYears = 7 * humanYears; //equation to calculate sparkys age in dog years using constant of 7
-console.log("Sparky is " + humanYears + "years years old which is " + dogYears +" in dog years") //prints answer in console
+//var humanYears = 5; //sparky is 5 years old in human years (given)
+//var dogYears = 7 * humanYears; //equation to calculate sparkys age in dog years using constant of 7
+//console.log("Sparky is " + humanYears + "years years old which is " + dogYears +" in dog years") //prints answer in console
 
 
 //Slices of Pie Part 1
-//var pizzaSlices = 10; //variable of how many slices there are in each pizza
-//var guests = 14; //variable of how many guests there are
-//var pizzaOrdered = 5; // variable of how many pizzas were ordered
-//var slicesPerPerson = pizzaSlices * pizzaOrdered / guests; //equation showing how many of slices of person each guest will get
-//console.log(slicesPerPerson); //prints answer to console
+var pizzaSlices = 10; //variable of how many slices there are in each pizza
+var guests = 14; //variable of how many guests there are
+var pizzaOrdered = 5; // variable of how many pizzas were ordered
+var slicesPerPerson = pizzaSlices * pizzaOrdered / guests; //equation showing how many of slices of person each guest will get
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party."); //prints answer to console
 
 
