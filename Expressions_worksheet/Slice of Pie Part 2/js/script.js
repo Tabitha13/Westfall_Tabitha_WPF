9a51ab6c4d8c67b1bfecdfd2eda52b17f6e0506e
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 13:25:07 -0500

    Slices of pizza part 2

---
+++ Expressions_worksheet/Slice of Pie Part 2/js/script.js
@@ -0,0 +1,7 @@
+// Tabitha Westfall February 9th 2015 Expressions Worksheet
+//Slices of Pie part 2
+var pizzaSlices = 10; //variable of how many slices there are in each pizza
+var guests = 14; //variable of how many guests there are
+var pizzaOrdered = 5; // variable of how many pizzas were ordered
+var remainder = pizzaSlices * pizzaOrdered % guests; //equation to determine how many slices sparky will get
+console.log ("Sparky got " + remainder + " slices of pizza.");
