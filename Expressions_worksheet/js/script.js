b53ac7afcdca99288c1f87000a7b01addd72d96f
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 12:50:21 -0500

    dog years

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,4 +2,8 @@
 //Dog Years
 var humanYears = 5; //sparky is 5 years old in human years (given)
 var dogYears = 7 * humanYears; //equation to calculate sparkys age in dog years using constant of 7
-console.log(dogYears);
+console.log(dogYears); //prints answer in the console
+
+
+
+
