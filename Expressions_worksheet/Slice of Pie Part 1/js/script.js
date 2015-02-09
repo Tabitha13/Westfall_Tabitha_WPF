99de51285a035523f569f10d2a3bc4d46944794b
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 13:16:29 -0500

    Slice of Pie part 1

--- Expressions_worksheet/Slice of Pie Part 1/js/script.js
+++ Expressions_worksheet/Slice of Pie Part 1/js/script.js
@@ -1,9 +1,4 @@
 // Tabitha Westfall February 9th 2015 Expressions Worksheet
-//Dog Years
-//var humanYears = 5; //sparky is 5 years old in human years (given)
-//var dogYears = 7 * humanYears; //equation to calculate sparkys age in dog years using constant of 7
-//console.log("Sparky is " + humanYears + "years years old which is " + dogYears +" in dog years") //prints answer in console
-
 
 //Slices of Pie Part 1
 var pizzaSlices = 10; //variable of how many slices there are in each pizza
