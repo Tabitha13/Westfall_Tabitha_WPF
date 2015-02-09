96ac012d8f6c8ae56c7b4aa62a8a7cb54e4701ef
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-09 13:45:07 -0500

    Average shopping bill

---
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -0,0 +1,7 @@
+// Tabitha Westfall February 9th 2015 Expressions Worksheet
+//Average shopping bill
+
+var weeklyAmounts = [120, 116, 142, 109, 125]; //weekly shopping totals
+var weeklyTotal = weeklyAmounts[0] + weeklyAmounts[1] + weeklyAmounts[2] + weeklyAmounts[3] + weeklyAmounts[4]; //adds up totals
+var weeklyAverage = weeklyTotal / 5; //finds average
+console.log("You have spent a total of $" + weeklyTotal + " on groceries over 5 weeks. That is an average of $" + weeklyAverage + " per week."); //displays answer in console
