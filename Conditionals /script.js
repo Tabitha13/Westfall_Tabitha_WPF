b3f1b2637c86bf3b96773fd52e6f3cfc80c35d3f
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 15:49:10 -0500

    adding expressions

--- Conditionals /script.js
+++ Conditionals /script.js
@@ -29,13 +29,13 @@
 //}
 
 //Conditional Logic
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
 //if child is old enough print to the console "you can ride"
 //if kid is  over 48 inches in height
- if(kidHeight > minHeight ){
+ if(kidHeight + 2 > minHeight ){
    //code performed if condition is true
    console.log("You can ride the coaster!");
  }
 
-//Relational Expressions
+
