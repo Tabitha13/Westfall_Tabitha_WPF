e6c91beed340223f1f26f2c299bdff846651690e
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 15:21:21 -0500

    examples

--- Conditionals /script.js
+++ Conditionals /script.js
@@ -1,8 +1,11 @@
 //Tabitha Westfall 2/17 Conditionals Learning Exercises
 
-sunny=true
-if(sunny){
+//sunny=true
+//if(sunny){
+    //goToTheBeach();
+//}else{}
+
+temp = 82
+if(temp > 70){
     goToTheBeach();
-}else{
-    
 }
