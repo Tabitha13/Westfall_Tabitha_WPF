df7da587b494482432d3561e0b5742ef57dea881
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 15:24:28 -0500

    nesting

--- Conditionals /script.js
+++ Conditionals /script.js
@@ -1,11 +1,23 @@
 //Tabitha Westfall 2/17 Conditionals Learning Exercises
 
+//logic
 //sunny=true
 //if(sunny){
     //goToTheBeach();
 //}else{}
 
-temp = 82
-if(temp > 70){
+//temp = 82
+//if(temp > 70){
+  //  goToTheBeach();
+//}
+
+//nesting
+if(sunny){
     goToTheBeach();
+
+    if(warmWater) {
+        wearNewSuit();
+    }
+} else{
+    goToTheMovies();
 }
