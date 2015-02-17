5cd110456dffc8b136c9fdd551f87c2c81e61bad
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 17:05:25 -0500

    commit 1

---
+++ Westfall_Tabitha_Conditionals_Assignment/js/script.js
@@ -0,0 +1,9 @@
+// Tabitha Westfall February 17th 2015 Conditionals Assignment
+
+//varibles
+var pairsOfShoes = prompt("How many shoes do you own?");
+var donateShoes = 4;
+var netWorth = true;
+
+//confirm boolean variable
+netWorth = confirm("If you have at least $100 click OK, if not click cancel");
