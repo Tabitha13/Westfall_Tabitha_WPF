ca961549010a77bfbcd26a1e9055ea392e799b7e
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 16:26:02 -0500

    logical

--- Conditionals /script.js
+++ Conditionals /script.js
@@ -29,17 +29,28 @@
 //}
 
 //Conditional Logic
-var kidHeight = 47;
-var minHeight = 48;
-var wParentHeight = 45; //the height of kid with parents
+//var kidHeight = 47;
+//var minHeight = 48;
+//var wParentHeight = 45; //the height of kid with parents
 //if child is old enough print to the console "you can ride"
 //if kid is  over 48 inches in height
- if(kidHeight > minHeight ){
+ //if(kidHeight > minHeight ){
    //code performed if condition is true
-   console.log("You can ride the coaster!");
- } else if(kidHeight > wParentHeight){
+   //console.log("You can ride the coaster!");
+ //} else if(kidHeight > wParentHeight){
      //you can ride if parent present
-    console.log("You can ride but only with a parent present");
+//    console.log("You can ride but only with a parent present");
+//}else{
+//     console.log("Sorry kid, you've got some growing to do first");
+//}
+
+//Logical Operators
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+//if price of phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && payCheck > 300){
+    console.log("We can buy the phone");
 }else{
-     console.log("Sorry kid, you've got some growing to do first");
+    console.log("NO phone for you");
 }
