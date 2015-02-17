c429d6bcb78fa1e5fc245091c4b17833503a22c2
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 15:52:04 -0500

    if if

--- Conditionals /script.js
+++ Conditionals /script.js
@@ -33,9 +33,11 @@ var kidHeight = 47;
 var minHeight = 48;
 //if child is old enough print to the console "you can ride"
 //if kid is  over 48 inches in height
- if(kidHeight + 2 > minHeight ){
+ if(kidHeight > minHeight ){
    //code performed if condition is true
    console.log("You can ride the coaster!");
  }
 
-
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first");
+}
