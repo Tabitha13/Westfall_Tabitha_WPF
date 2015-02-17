2957106e9c36c9a6100880ad9309136dd1c5abab
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 17:39:50 -0500

    before testing

--- Westfall_Tabitha_Conditionals_Assignment/js/script.js
+++ Westfall_Tabitha_Conditionals_Assignment/js/script.js
@@ -8,5 +8,21 @@ var netWorth = true;
 netWorth = confirm("If you have at least $100 click OK, if not click cancel");
 
 if(netWorth === true) {
+    var pairsOfShoes = prompt("How many shoes do you own?");
 
+        if(pairsOfShoes <= donateShoes && netWorth >= 100) {
+            console.log("You can buy more shoes!");
+        }else{
+            var donateAmount = pairsOfShoes - donateShoes;
+            console.log("You need to donate " + donateAmount + " pairs of shoes first.");
+        }
+
+}else{
+    var actualNetWorth = prompt("How much money do you have?");
+
+        if(actualNetWorth === ""){
+            console.log("Hey you didn't enter how much money you have")
+        }else{
+            var saveAmount
+        }
 }
