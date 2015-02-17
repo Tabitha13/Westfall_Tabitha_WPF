73322a7c4f2e4ac6bd41ce22d99e54ed01dbc7a9
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 17:58:25 -0500

    adding ternary

--- Westfall_Tabitha_Conditionals_Assignment/js/script.js
+++ Westfall_Tabitha_Conditionals_Assignment/js/script.js
@@ -7,24 +7,39 @@ var netWorth = true;
 //confirm boolean variable
 netWorth = confirm("If you have at least $100 click OK, if not click cancel");
 
-if(netWorth === true) {
-    var pairsOfShoes = prompt("How many shoes do you own?");
+if(netWorth === true) { //this runs if i click ok for having $100
+    var pairsOfShoes = prompt("How many shoes do you own?"); //prompt to give shoe varialbe
 
-        if(pairsOfShoes <= donateShoes && netWorth <= 100) {
+        if(pairsOfShoes <= donateShoes && netWorth <= 100) { //logical conditional
             console.log("You can buy more shoes!");
         }else{
-            var donateAmount = pairsOfShoes - donateShoes;
+            var donateAmount = pairsOfShoes - donateShoes; //math
             console.log("You need to donate " + donateAmount + " pairs of shoes first.");
         }
 
-}else{
-    var actualNetWorth = prompt("How much money do you have?");
-
-        if(actualNetWorth === ""){
-            console.log("Hey you didn't enter how much money you have")
-        }else{
-            var moneyNeed = 100;
-            var saveAmount = moneyNeed - actualNetWorth;
-            console.log("You need to save $" + saveAmount + " before you can get more shoes.");
-        }
+}else{ //this runs if i click cancel
+    var actualNetWorth = prompt("How much money do you have?"); //finds out how much money i have
+    var moneyNeed = 100;
+    var saveAmount = moneyNeed - actualNetWorth; //math
+    (actualNetWorth) ? console.log("You need to save $" + saveAmount + " before you can get more shoes.") : console.log("Hey you didn't enter how much money you have");
 }
+
+
+
+
+
+
+
+
+
+//}else{ //this runs if i click cancel
+  //  var actualNetWorth = prompt("How much money do you have?"); //finds out how much money i have
+
+ //   if(actualNetWorth === ""){
+  //      console.log("Hey you didn't enter how much money you have"); //runs if i leave something blank
+  //  }else{
+   //     var moneyNeed = 100;
+  //      var saveAmount = moneyNeed - actualNetWorth; //math
+  //      console.log("You need to save $" + saveAmount + " before you can get more shoes."); //tells me how much money i have to save
+ //   }
+//}
