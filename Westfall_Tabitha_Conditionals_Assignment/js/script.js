92a7dd8e5588f47854a1b0d26cc3121bb8f67402
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-17 17:10:20 -0500

    adding comments

--- Westfall_Tabitha_Conditionals_Assignment/js/script.js
+++ Westfall_Tabitha_Conditionals_Assignment/js/script.js
@@ -9,5 +9,10 @@ var netWorth = true;
 netWorth = confirm("If you have at least $100 click OK, if not click cancel");
 
 if(netWorth === true && pairsOfShoes <= donateShoes){
+//this code will run if i have at least $100 and less than or equal to 4 pairs of shoes
     console.log("You can buy more shoes!!")
+    }else{
+    //runs if i dont have both $100 and 4 pairs of shoes or less
+    console.log("Maybe you should hold off on getting new shoes for now.")
 }
+
