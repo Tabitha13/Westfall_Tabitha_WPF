b1777291371f9c18137a4d706824202397221a3f
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-22 12:34:27 -0500

    explain

--- Lynda Loops/script.js
+++ Lynda Loops/script.js
@@ -1,11 +1,20 @@
 //Tabitha Westfall 2/22 Loops
 
- var a = 100;
+ //var a = 100;
 
-while (a < 10) { //true
-    console.log(a); //infinite loop (bad)
-    a++; //increments a to 10
+//while (a < 10) { //true
+ //   console.log(a); //infinite loop (bad)
+ //   a++; //increments a to 10
 
-} while ( a < 10); //block will always be executed at least once before it is checked
+//} while ( a < 10); //block will always be executed at least once before it is checked
 
- 
+
+var i =1; //set up the index
+
+while (i < 10) { //checks the condition
+    //do stuff
+    //do stuff
+    //do stuff
+    //etc...
+    i++; // increments the index
+}
