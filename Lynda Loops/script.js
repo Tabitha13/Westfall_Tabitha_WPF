e13f91bc92f82790e7ed840c3b1a0da3b9e8af39
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-22 12:32:27 -0500

    do loop

--- Lynda Loops/script.js
+++ Lynda Loops/script.js
@@ -1,9 +1,11 @@
 //Tabitha Westfall 2/22 Loops
 
- var a = 1;
+ var a = 100;
 
 while (a < 10) { //true
     console.log(a); //infinite loop (bad)
     a++; //increments a to 10
 
-}
+} while ( a < 10); //block will always be executed at least once before it is checked
+
+ 
