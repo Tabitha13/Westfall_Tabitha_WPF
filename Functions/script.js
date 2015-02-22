a1cacf0ac02e36918e9780ebf9cb1d02660f287d
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-22 13:14:16 -0500

    basic

---
+++ Functions/script.js
@@ -0,0 +1,27 @@
+//Tabitha Westfall Functions 2/22
+
+// functions allow us to have code that is written once, but able to be used again and again by the machine
+
+//functions store stuff
+//ex. plants vs zombies -- used to make plant receptively shoot zombie
+
+//Types of Functions
+// Functions - normal functions
+// Procedures
+//Anonymous
+
+//normal function
+//function functionName() { //parenthesis can be left empty
+    //code the function runs
+//}
+
+function outputMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
