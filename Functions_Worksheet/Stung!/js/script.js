b23572612c3c658c05ca27ab85cd5f2eeac97c1a
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 14:30:20 -0500

    given

--- Functions_Worksheet/Stung!/js/script.js
+++ Functions_Worksheet/Stung!/js/script.js
@@ -1,6 +1,7 @@
 // Tabitha Westfall 2/23 Functions Worksheet - Stung!
 
 var victimsWeight = 120; //given
+var stings = 8.666666667; //given 
 
-var beeStings = calcStings(weight, stings); // argument
+var beeStings = calcStings(victimWeight, stings); // argument
 
