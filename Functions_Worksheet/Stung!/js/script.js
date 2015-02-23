8b4d1f7a780fe3fa976f1181f1f50aa54481bdc4
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 14:37:04 -0500

    comments

--- Functions_Worksheet/Stung!/js/script.js
+++ Functions_Worksheet/Stung!/js/script.js
@@ -4,9 +4,9 @@ var victimWeight = 120; //given
 var stings = 8.666666667; //given
 
 var beeStings = calcStings(victimWeight, stings); // argument
-console.log("It takes " +  Math.round(beeStings) + " bee stings to kill this animal.");
+console.log("It takes " +  Math.round(beeStings) + " bee stings to kill this animal."); // prints to console
 
 function calcStings(vW, s ){ //parameters
-    return vW* s;
+    return vW* s; //equation to find bee stings
 
 }
