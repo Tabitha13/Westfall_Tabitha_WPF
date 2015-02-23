0ae3d44d75c3ee348b2e45a70e0f67d8d2d10c4c
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 14:36:02 -0500

    fix decimal round

--- Functions_Worksheet/Stung!/js/script.js
+++ Functions_Worksheet/Stung!/js/script.js
@@ -4,7 +4,7 @@ var victimWeight = 120; //given
 var stings = 8.666666667; //given
 
 var beeStings = calcStings(victimWeight, stings); // argument
-console.log("It takes " + beeStings + " bee stings to kill this animal.");
+console.log("It takes " +  Math.round(beeStings) + " bee stings to kill this animal.");
 
 function calcStings(vW, s ){
     return vW* s;
