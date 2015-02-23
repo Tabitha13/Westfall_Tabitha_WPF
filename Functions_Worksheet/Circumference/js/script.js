82b0d288f2580c9752602aaf6649afb54c6f5859
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 13:50:52 -0500

    circumference

---
+++ Functions_Worksheet/Circumference/js/script.js
@@ -0,0 +1,8 @@
+//Tabitha Westfall 2/23 Functions Worksheet- Circumference
+
+var radius = 5;
+var pi = 3.14;
+
+var circ = calcCirc(radius, pi);
+console.log("The circumference of the circle is " + circ + ".");
+
