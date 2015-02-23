155217f9a8130e4cdce418a78a5a4d0ee4d340da
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 14:32:37 -0500

    setting up equation

--- Functions_Worksheet/Stung!/js/script.js
+++ Functions_Worksheet/Stung!/js/script.js
@@ -1,7 +1,11 @@
 // Tabitha Westfall 2/23 Functions Worksheet - Stung!
 
 var victimsWeight = 120; //given
-var stings = 8.666666667; //given 
+var stings = 8.666666667; //given
 
 var beeStings = calcStings(victimWeight, stings); // argument
+console.log("It takes " + beeStings + " to kill this animal.");
 
+function calcStings(vW, s ){
+    return vW* s;
+}
