e7fd7eca1c066cc7c69d22e9819b1c78732073ad
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-23 14:21:06 -0500

    comments more

--- Functions_Worksheet/Circumference/js/script.js
+++ Functions_Worksheet/Circumference/js/script.js
@@ -4,9 +4,9 @@ var radius = 5; //given
 var pi = 3.14; // given
 
 
-var circ = calcCirc(radius, pi); //result variable
-console.log("The circumference of the circle is " + circ + ".");
+var circ = calcCirc(radius, pi); //result variable //arguements
+console.log("The circumference of the circle is " + circ + "."); //print circumference to console
 
-function calcCirc(r, p){
-    return 2 * r * p;
+function calcCirc(r, p){ //parameters
+    return 2 * r * p; //equation to find circ
 }
