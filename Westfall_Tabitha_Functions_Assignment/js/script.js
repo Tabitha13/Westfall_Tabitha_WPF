1295596cc36b8f07bc823ee85965bd1262d39945
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 13:56:32 -0500

    setting up if statement for lottery mums

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -1,9 +1,23 @@
 // Tabitha Westfall 2/24 Functions Assignment- Powerball or Florida Lottery
 
+
 var choice = true;
 
 choice = confirm("Hello! If you would like numbers for the Florida Lottery please click OK, or click Cancel for Powerball Numbers.");
 
 if(choice === true) {
+
+    function randomLotteryNum(min, max){
+
+        for (var i = 0; i < 6; i++){
+            var lottery = math.random() * (max - min) + min;
+
+            lottery = math.round(lottery);
+        }
+
+        return lottery;
+    }
+
+}else{
 
 }
