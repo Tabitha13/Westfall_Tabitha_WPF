18a2ab6f728e7fec022e785a48bfb3102f129dee
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 14:28:03 -0500

    comments

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -14,37 +14,37 @@ if(choice === true) {
         for (var i = 0; i < 6; i++){ // will run i until it equals 6
             var lottery = Math.random() * (max - min) + min; //set up random number generator
 
-            lottery = Math.round(lottery); //
-            lotteryArray[i] = lottery;
+            lottery = Math.round(lottery); // rounds numbers to whole nums
+            lotteryArray[i] = lottery; //will display the array
 
         }
 
         return lotteryArray;
 
     }
-    myNum = randomLotteryNum(1, 53);
+    myNum = randomLotteryNum(1, 53); //parameters
 
-    console.log("Your random Lottery Numbers are: " + myNum  );
+    console.log("Your random Lottery Numbers are: " + myNum  ); //display nums
 
-}else{
-    function randomPowerballNum(min, max){
+}else{ //happens if user clicks cancel
+    function randomPowerballNum(min, max){ //arguments
 
-        var powerballArray = [];
+        var powerballArray = []; //will give me 5 nums
 
-        for (var i = 0; i < 5; i++){
-            var powerball = Math.random() * (max - min) + min;
+        for (var i = 0; i < 5; i++){ //will run until i equals 5
+            var powerball = Math.random() * (max - min) + min; //set up random num
 
-            powerball = Math.round(powerball);
-            powerballArray [i] = powerball;
+            powerball = Math.round(powerball); //rounds nums to wholes
+            powerballArray [i] = powerball; // will display array
 
         }
 
         return powerballArray;
 
     }
-    myNum = randomPowerballNum(1, 59);
+    myNum = randomPowerballNum(1, 59); //parameters
 
-    console.log("Your random Powerball Numbers are: " +myNum  );
+    console.log("Your random Powerball Numbers are: " +myNum  ); //display nums
 
 }
 
