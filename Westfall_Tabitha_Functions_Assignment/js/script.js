e22415a60c4fa7cfbdee5b1aa9cb549a612c25ee
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 13:24:46 -0500

    simple choice
    make choice simple

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -1,10 +1,26 @@
 // Tabitha Westfall 2/24 Functions Assignment- Powerball or Florida Lottery
 
-var choice = prompt("Hello! \nDo you want to Powerball numbers or Florida Lottery Numbers? \nPlease enter Powerball or Lottery"); //set up for function
+var choice = prompt("Hello! \nDo you want to Powerball numbers or Florida Lottery Numbers? \nPlease enter P for Powerball or L for Lottery"); //set up for function
 
 function choiceValid(theChoice){
-    while (theChoice === "") {
-        theChoice = prompt("Please make sure you are entering the correct spelling. \n Powerball or Lottery?");
+
+    var numOfTimes = 1;
+
+    while (theChoice === "" ) {
+
+        theChoice = prompt("Please make sure you are entering the correct spelling. \n P for Powerball or L for Lottery?");
+        numOfTimes = numOfTimes + 1;
+
+        if (numOfTimes === 5){
+            console.log("Please stop wasting my time.");
+            break;
+        }
 
     }
+
+    return theChoice;
+
 }
+
+choice = choiceValid(choice);
+console.log("Thank you for choosing " + choice + ".")
