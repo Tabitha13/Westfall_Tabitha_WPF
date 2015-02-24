f205196e434a0ee261127a4dc56f6b377e727d36
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 14:29:43 -0500

    make prompt prettier

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -3,7 +3,7 @@
 
 var choice = true;
 
-choice = confirm("Hello! If you would like numbers for the Florida Lottery please click OK, or click Cancel for Powerball Numbers.");
+choice = confirm("Hello! If you would like numbers for the \nFlorida Lottery please click OK, \nor click Cancel for Powerball Numbers.");
 
 if(choice === true) {
 
