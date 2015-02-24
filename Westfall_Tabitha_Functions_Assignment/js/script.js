92e9a32d98bd323b1ae1e7d5b40301e094cbb905
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 13:40:42 -0500

    re-do

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -1,26 +1,9 @@
 // Tabitha Westfall 2/24 Functions Assignment- Powerball or Florida Lottery
 
-var choice = prompt("Hello! \nDo you want to Powerball numbers or Florida Lottery Numbers? \nPlease enter P for Powerball or L for Lottery"); //set up for function
+var choice = true;
 
-function choiceValid(theChoice){
+choice = confirm("Hello! If you would like numbers for the Florida Lottery please click OK, or click Cancel for Powerball Numbers.");
 
-    var numOfTimes = 1;
-
-    while (theChoice === "" ) {
-
-        theChoice = prompt("Please make sure you are entering the correct spelling. \n P for Powerball or L for Lottery?");
-        numOfTimes = numOfTimes + 1;
-
-        if (numOfTimes === 5){
-            console.log("Please stop wasting my time.");
-            break;
-        }
-
-    }
-
-    return theChoice;
+if(choice === true) {
 
 }
-
-choice = choiceValid(choice);
-console.log("Thank you for choosing " + choice + ".")
