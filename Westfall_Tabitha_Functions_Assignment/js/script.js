9193f01861c29fd2e58aa1cc376941d1604ee5ca
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 14:09:28 -0500

    commit before testing

--- Westfall_Tabitha_Functions_Assignment/js/script.js
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -9,15 +9,44 @@ if(choice === true) {
 
     function randomLotteryNum(min, max){
 
-        for (var i = 0; i < 6; i++){
-            var lottery = math.random() * (max - min) + min;
+        var lotteryArray = [];
 
-            lottery = math.round(lottery);
+        for (var i = 0; i < 6; i++){
+            var lottery = Math.random() * (max - min) + min;
+
+            lottery = Math.round(lottery);
+            lotteryArray[i] = lottery;
+
         }
 
-        return lottery;
+        return lotteryArray;
+
     }
+    myNum = randomLotteryNum(1, 53);
+
+    console.log("Your random Lottery Numbers are: " +myNum  );
 
 }else{
+    function randomPowerballNum(min, max){
+
+        var powerballArray = [];
+
+        for (var i = 0; i < 5; i++){
+            var powerball = Math.random() * (max - min) + min;
+
+            powerball = Math.round(lottery);
+            powerballArray [i] = powerball;
+
+        }
+
+        return powerballArray;
+
+    }
+    myNum = randomPowerballNum(1, 59);
+
+    console.log("Your random Powerball Numbers are: " +myNum  );
 
 }
+
+//main code
+lotterNum = randomLotteryNum(1, 53);
