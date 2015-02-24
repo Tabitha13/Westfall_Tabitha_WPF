23b5cdb96e25870149923606b2cc8a0d9095ab56
Tabitha13 <tnwestfall@fullsail.edu>
2015-02-24 13:03:17 -0500

    first commit

---
+++ Westfall_Tabitha_Functions_Assignment/js/script.js
@@ -0,0 +1,4 @@
+// Tabitha Westfall 2/24 Functions Assignment- Powerball or Florida Lottery
+
+var choice = prompt("Hello! \nDo you want to Powerball numbers or Florida Lottery Numbers? \nPlease enter Powerball or Lottery"); //set up for function
+
